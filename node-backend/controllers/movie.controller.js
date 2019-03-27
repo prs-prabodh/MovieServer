6c5f4f2c3ae8bfaa99ea943df2c36eada2baf31c
prs-prabodh <prabodhranjan98@gmail.com>
2019-03-27 23:02:38 +0530

    Passed array of actors from backend

--- node-backend/controllers/movie.controller.js
+++ node-backend/controllers/movie.controller.js
@@ -12,6 +12,15 @@ exports.search = function (req, res) {
     // console.log(exp);
     movie.find({ 'name': exp }, function (error, movies) {
         if (error) console.log(error);
-        res.json(movies);
+        else{
+            var editedMovies = [];
+            movies.forEach(function(movie) {
+                newMovie = movie.toObject();
+                newMovie.actors = [movie.actor_1_name, movie.actor_2_name];
+                delete newMovie.actor_1_name, newMovie.actor_2_name;
+                editedMovies.push(newMovie);
+            });
+            res.json(editedMovies.slice(0,11));
+        }
     });
 };
